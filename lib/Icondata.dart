93d79da1a75b0f63aac7f443420e5b5766a3f31b
abhinavuppu7 <abhinavuppu7@gmail.com>
2021-06-16 22:42:08 +0530

    icon data in seperate file

---
+++ lib/Icondata.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Map<String, IconData> catmap = {
+  'sports': Icons.sports_baseball_outlined,
+  'food': Icons.restaurant_outlined,
+  'grocery': Icons.local_grocery_store_outlined,
+  'shopping': Icons.shopping_bag_outlined,
+  'travel': Icons.drive_eta_outlined,
+  'fitness': Icons.fitness_center_outlined,
+  'bills': Icons.receipt_long_outlined,
+  'housing': Icons.home_outlined,
+  'health': Icons.local_hospital_outlined,
+};
+Map<String, Color> catcol = {
+  'sports': Colors.purple,
+  'food': Colors.red,
+  'grocery': Colors.green,
+  'shopping': Colors.blue,
+  'travel': Colors.yellow,
+  'fitness': Colors.orange,
+  'bills': Colors.cyan,
+  'housing': Colors.teal,
+  'health': Colors.lime,
+};
