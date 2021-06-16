28a8e3bd4592bab56729d5ddbb7cb2c1ca5a3c6a
abhinavuppu7 <abhinavuppu7@gmail.com>
2021-06-16 22:45:15 +0530

    refactored code

--- lib/screens/Homescreen.dart
+++ lib/screens/Homescreen.dart
@@ -1,9 +1,12 @@
+import 'package:cashflow/components/Reusablecard.dart';
+import 'package:cashflow/components/Transactiondetailcard.dart';
 import 'package:cashflow/screens/AddTransaction.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 import 'package:cashflow/helpers/database_connection.dart';
 import 'package:cashflow/datamodels/tranasactionmodel.dart';
+import '../Icondata.dart';
 
 class Homescreen extends StatefulWidget {
   @override
@@ -13,45 +16,33 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   int amount = 0, income = 0, expense = 0;
   Future<List<Transactions>> _transactionList;
-  Future<List> amounthelper;
   Future<List> incomehelper;
   Future<List> expensehelper;
-  List amounts;
   List incomes;
   List expenses;
+
+  final DateFormat _dateformatter = DateFormat('MMM dd,yyyy');
 
   @override
   void initState() {
     super.initState();
-    //   _getAmount();
+
     _UpdateTransactionList();
     _UpdateAmount();
   }
 
-  _getAmount() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    amount = prefs.getInt('cashbalance');
-    setState(() {
-      amount;
-    });
-  }
-
   _UpdateAmount() async {
-    amounthelper = DataBaseConnection.instance.getSum();
     incomehelper = DataBaseConnection.instance.getIncome();
     expensehelper = DataBaseConnection.instance.getExpense();
-
-    amounts = await amounthelper;
-    print(amounts[0]['sum_count']);
     incomes = await incomehelper;
     print(incomes[0]['sum_income']);
-
     expenses = await expensehelper;
     print(expenses[0]['sum_expense']);
     setState(() {
-      amount = amounts[0]['sum_count'];
-      income = incomes[0]['sum_income'];
-      expense = expenses[0]['sum_expense'];
+      income = incomes[0]['sum_income'] == null ? 0 : incomes[0]['sum_income'];
+      expense =
+          expenses[0]['sum_expense'] == null ? 0 : expenses[0]['sum_expense'];
+      amount = income - expense;
     });
   }
 
@@ -62,36 +53,29 @@ class _HomescreenState extends State<Homescreen> {
   }
 
   Widget _buildTransaction(Transactions transaction) {
-    return Container(
-      height: 57,
-      margin: EdgeInsets.only(left: 15, bottom: 15, right: 15),
-      padding: EdgeInsets.only(left: 24, top: 12, bottom: 12, right: 22),
-      decoration: BoxDecoration(
-        // color: Color(0xfffcf1f1),
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(15),
-        boxShadow: [
-          BoxShadow(
-            color: Color(0xFF3A3A3A),
-            blurRadius: 10,
-            spreadRadius: 5,
-            offset: Offset(8.0, 8.0),
-          )
-        ],
-      ),
-      child: ListTile(
-        title: Text(transaction.description),
-        trailing: Text('${transaction.amount}'),
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (_) => AddTransaction(
-                        UpdateTransactionList: _UpdateTransactionList,
-                        UpdateAmount: _UpdateAmount(),
-                        transaction: transaction,
-                      )));
-        },
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (_) => AddTransaction(
+                      UpdateTransactionList: _UpdateTransactionList,
+                      UpdateAmount: _UpdateAmount,
+                      transaction: transaction,
+                    )));
+      },
+      child: TransactionTile(
+        catmap: catmap,
+        description: transaction.description,
+        transactiontype: transaction.transactiontype,
+        transactionamount: transaction.amount,
+        transactioncategory: transaction.category,
+        transactiondate: _dateformatter.format(transaction.date),
+        catcol: catcol,
+        transaction: transaction,
+
+        // updateAmount: _UpdateAmount(),
+        // updatetransactionList: _UpdateTransactionList(),
       ),
     );
   }
@@ -100,7 +84,7 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).primaryColor,
+          backgroundColor: Colors.greenAccent,
           child: Icon(
             Icons.add,
           ),
@@ -121,47 +105,38 @@ class _HomescreenState extends State<Homescreen> {
             );
           }
           return ListView.builder(
-              padding: EdgeInsets.symmetric(vertical: 80.0),
               itemCount: 1 + snapshot.data.length,
               itemBuilder: (BuildContext context, int index) {
                 if (index == 0) {
-                  return Padding(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 40, vertical: 20.0),
+                  return Container(
                     child: Column(children: <Widget>[
                       Row(children: <Widget>[
                         Expanded(
-                            child: Container(
-                          child: Column(
-                            children: <Widget>[
-                              Text('mount'),
-                              Text('${amount}')
-                            ],
-                          ),
-                        )),
+                            child: DetailCard(
+                                label: "CASH BALANCE",
+                                value: amount,
+                                colour: Colors.green)),
                       ]),
                       Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Expanded(
-                              child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Text('Income'),
-                                Text('${income}')
-                              ],
-                            ),
+                              child: DetailCard(
+                            label: "INCOME",
+                            value: income,
+                            colour: Colors.orange,
                           )),
                           Expanded(
-                              child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Text('Expenses'),
-                                Text('${expense}')
-                              ],
-                            ),
-                          ))
+                            child: DetailCard(
+                                label: "EXPENSES",
+                                value: expense,
+                                colour: Colors.red),
+                          )
                         ],
+                      ),
+                      Text(
+                        'My Transactions',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
                       )
                     ]),
                   );
