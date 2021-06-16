962594c756c892e7f4ccbafc099ec5dc88c0cc7f
abhinavuppu7 <abhinavuppu7@gmail.com>
2021-06-16 22:45:41 +0530

    theme changed  to  dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:cashflow/screens/Firstscreen.dart';
+import 'package:cashflow/screens/Homescreen.dart';
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/ABHINAV/AndroidStudioProjects/cashflow/lib/screens/Homescreen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -16,44 +13,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool amountexists;
   @override
-  //  void initState() {
-  //    // TODO: implement initState
-  //    super.initState();
-  // getData();
-  //  }
-  //  getData() async{
-  //    SharedPreferences prefs= await SharedPreferences.getInstance();
-  // setState(() {
-  //   amountexists=prefs.containsKey('cashbalance');
-  // });
-
-  // }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+      theme: ThemeData.dark(),
       home: Homescreen(),
     );
   }
 }
-
-//
-// class MyApp extends StatefulWidget {
-//   // This widget is the root of your application.
-//   @override
-//
-//
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//      debugShowCheckedModeBanner: false,
-//       theme: ThemeData(
-//         primarySwatch: Colors.green,
-//       ),
-//
-//       home: Homescreen(),
-//     );
-//   }
-// }
