a56d90f3b63a2c15b54219b2edb46e649fd6e93f
abhinavuppu7 <abhinavuppu7@gmail.com>
2021-06-16 22:44:16 +0530

    added date and category

--- lib/datamodels/tranasactionmodel.dart
+++ lib/datamodels/tranasactionmodel.dart
@@ -1,46 +1,48 @@
-
-
-
-class Transactions{
-  static  String table="transactions";
+class Transactions {
+  static String table = "transactions";
   int id;
 
   String transactiontype;
   String description;
+  String category;
+  DateTime date;
 
   int amount;
 
-  Transactions ({this.amount,this.description,this.transactiontype});
- Transactions.WithID({this.id,this.amount,this.description,this.transactiontype});
+  Transactions(
+      {this.amount,
+      this.description,
+      this.transactiontype,
+      this.date,
+      this.category});
+  Transactions.WithID(
+      {this.id,
+      this.amount,
+      this.description,
+      this.transactiontype,
+      this.date,
+      this.category});
 
- factory Transactions.fromMap(Map<String,dynamic>map){
-   return Transactions.WithID(
-     id: map['id'],
-     transactiontype: map['transactiontype'],
-     description: map['description'],
-     amount: map['amount'],
-   );
-
+  factory Transactions.fromMap(Map<String, dynamic> map) {
+    return Transactions.WithID(
+      id: map['id'],
+      transactiontype: map['transactiontype'],
+      description: map['description'],
+      amount: map['amount'],
+      date: DateTime.parse(map['date']),
+      category: map['category'],
+    );
   }
 
-//   Transactions.fromMap(Map<String,dynamic>map){
-//    id=map[DataBaseConnection.COLUMN_ID];
-//    amount=map[DataBaseConnection.COLUMN_AMOUNT];
-// transactiontype=map[DataBaseConnection.COLUMN_TRANSACTIONTYPE];
-//
-//   }
   Map<String, dynamic> toMap() {
-    final map=Map<String,dynamic>();
-    if(id!=null)
-      map['id']=id;
+    final map = Map<String, dynamic>();
+    if (id != null) map['id'] = id;
 
-    map['transactiontype']=transactiontype;
-    map['description']=description;
-    map['amount']=amount;
+    map['transactiontype'] = transactiontype;
+    map['description'] = description;
+    map['amount'] = amount;
+    map['category'] = category;
+    map['date'] = date.toIso8601String();
     return map;
   }
-
-
-
 }
-
